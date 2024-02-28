49bfc721e2d6647a78b2893f2e87cc092076e613
TheColdGuy251 <smirnovilya7031@gmail.com>
2024-02-28 23:56:27 +0500

    login update

--- main.py
+++ main.py
@@ -76,17 +76,22 @@ def reqister():
 # вход в аккаунт
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+    data = request.json.get('data')
+    auth = data.get('auth')
+    password = data.get('password')
+    db_sess = db_session.create_session()
+    if "@" in str(auth):
+        user = db_sess.query(User).filter(User.email == auth).first()
+        if user and user.check_password(password):
+            login_user(user, remember=True)
+            return jsonify({'success': "OK"})
+        return jsonify({'success': "WrongAuth"})
+    else:
+        user = db_sess.query(User).filter(User.username == auth).first()
+        if user and user.check_password(password):
+            login_user(user, remember=True)
+            return jsonify({'success': "OK"})
+        return jsonify({'success': "WrongAuth"})
 
 
 # выход из аккаунта
