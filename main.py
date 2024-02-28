39b5bfeca77cabdf1d1174d07f3091074c8052a2
TheColdGuy251 <smirnovilya7031@gmail.com>
2024-02-28 23:18:28 +0500

    register update

--- main.py
+++ main.py
@@ -44,37 +44,32 @@ def index():
 # регистрация аккаунта
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
-    form = RegisterForm()
     db_sess = db_session.create_session()
-    if form.validate_on_submit():
-        form.username.data = str(form.username.data).lower().strip()
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Пароли не совпадают")
-
-        if db_sess.query(User).filter(User.username == form.username.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Такое имя пользователя уже есть")
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Такой адрес электронной почты уже есть")
-        if " " in form.username.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Нельзя использовать пробел в имени")
-        user = User(
-            username=form.username.data,
-            surname=form.surname.data,
-            name=form.name.data,
-            patronymic=form.patronymic.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-
-    return render_template('register.html', title='Регистрация', form=form)
+    data = request.json.get('data')
+    username = data.get('username')
+    surname = data.get('surname')
+    name = data.form.get('name')
+    patronymic = data.get('patronymic')
+    about = data.get('about')
+    email = data.get('email')
+    password = data.get('password')
+    username = str(username).lower().strip()
+    if db_sess.query(User).filter(User.username == username).first():
+        return {'success': 'user_exists'}
+    if db_sess.query(User).filter(User.email == email).first():
+        return {'success': 'email_exists'}
+    user = User(
+        username=username,
+        surname=surname,
+        name=name,
+        patronymic=patronymic,
+        email=email,
+        about=about
+    )
+    user.set_password(password)
+    db_sess.add(user)
+    db_sess.commit()
+    return jsonify({'success': "OK"})
 
 
 # вход в аккаунт
