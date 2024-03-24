d881186fde1fe966b7138edd3057995e8eb8fdaa
TheColdGuy251 <smirnovilya7031@gmail.com>
2024-03-24 21:06:37 +0500

    added tasks

--- data/tasks.py
+++ data/tasks.py
@@ -11,13 +11,17 @@ class Task(SqlAlchemyBase, UserMixin, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     task_name = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
-    host_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     begin_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
-    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
-    priority = sqlalchemy.Column(sqlalchemy.Integer)
+    condition = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    priority = sqlalchemy.Column(sqlalchemy.Integer, default=1)
+    complete_perc = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    remind = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    date_remind = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    host_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     description = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
     def __repr__(self):
-        return f'<Task> {self.id} {self.task_name} {self.host_id} {self.begin_date} {self.end_date} {self.is_private} {self.description} {self.created_date}'
+        return f'<Task> {self.id} {self.task_name} {self.host_id} {self.begin_date} {self.end_date} {self.is_private} {self.description} {self.created_date} {self.date_remind} {self.remind} {self.condition} {self.priority}'
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, redirect, request, make_response, session, abort, jsonify, url_for, \
     send_from_directory
 from data import db_session
+from datetime import datetime, timedelta, timezone
 from data.users import User
 from data.tasks import Task
+from data.calendar import Calendar
 from forms.register_form import RegisterForm
 from forms.login_form import LoginForm
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from sqlalchemy import text
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, jwt_manager, get_jwt, set_access_cookies, unset_jwt_cookies
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tyuiu_secret_key'
 CORS(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
+jwt = JWTManager(app)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=14)
 
 def main():
     db_session.global_init("db/users.db")
@@ -31,40 +32,142 @@ def bad_request(_):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    db_sess = db_session.create_session()
-    if request.method == "POST":
-        data = request.json.get('data')
-        tasks = db_sess.query(Task).filter(Task.name == data.get("task_name")).all()
-        return jsonify({'success': "OK", 'tasks': tasks})
     return jsonify({'success': "OK"})
+
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
+
+
+@app.route("/tasks", methods=['GET'])
+@jwt_required()
+def load_task():
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        current_user = get_jwt_identity()
+        user = db_sess.query(User).filter(User.id == current_user).first()
+        if not user:
+            return jsonify({'error': "User not found"}), 404
+        tasks = db_sess.query(Task).filter(Task.host_id == user.id).all()
+        data = {}
+        for i, task in enumerate(tasks):
+            data[i] = {
+                "task_name": task.task_name,
+                "begin_date": task.begin_date,
+                "end_date": task.end_date,
+                "condition": task.condition,
+                "complete_perc": task.complete_perc,
+                "remind": task.remind,
+                "date_remind": task.date_remind,
+                "is_private": task.is_private,
+                "priority": task.priority,
+                "host_id": task.host_id,
+                "description": task.description,
+                "created_date": task.created_date
+            }
+        return jsonify({'success': True, 'data': data})
+
+
+@app.route("/tasks/add", methods=['POST'])
+@jwt_required()
+def addtask():
+    db_sess = db_session.create_session()
+    current_user = get_jwt_identity()
+    user = db_sess.query(User).filter(User.id == current_user).first()
+    if not user:
+        return jsonify({'error': "User not found"}), 404
+    data = request.json.get('data')
+    task = Task(
+        task_name=data.get('task_name'),
+        begin_date=datetime.strptime(data.get('begin_date'), '%d.%m.%Y').date(),
+        end_date=datetime.strptime(data.get('end_date'), '%d.%m.%Y').date(),
+        condition=data.get('condition'),
+        complete_perc=data.get('complete_perc'),
+        remind=bool(data.get('remind')),
+        date_remind=datetime.strptime(data.get('date_remind'), '%d.%m.%Y').date(),
+        is_private=bool(data.get('is_private')),
+        priority=data.get('priority'),
+        description=data.get('description'),
+        host_id=current_user
+    )
+    db_sess.add(task)
+    db_sess.commit()
+    return jsonify({'success': True})
+
+
+@app.route("/calendar", methods=['GET'])
+@jwt_required()
+def load_calendar():
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        current_user = get_jwt_identity()
+        user = db_sess.query(User).filter(User.id == current_user).first()
+        if not user:
+            return jsonify({'error': "User not found"}), 404
+        calendar = db_sess.query(Calendar).filter(Calendar.host_id == user.id).all()
+        data = {}
+        for i, task in enumerate(calendar):
+            data[i] = {
+                "task_name": task.task_name,
+                "begin_date": task.begin_date,
+                "end_date": task.end_date,
+                "is_private": task.is_private,
+                "host_id": task.host_id,
+            }
+        return jsonify({'success': True, 'data': data})
+
+
+@app.route("/tasks/add", methods=['POST'])
+@jwt_required()
+def add_calendar():
+    db_sess = db_session.create_session()
+    current_user = get_jwt_identity()
+    user = db_sess.query(User).filter(User.id == current_user).first()
+    task = db_sess.query(Task).filter(Task.host_id == current_user).first()
+    if not user:
+        return jsonify({'error': "User not found"}), 404
+    data = request.json.get('data')
+    calendar = Calendar(
+        task_name=task.task_name,
+        begin_date=datetime.strptime(data.get('begin_date'), '%d.%m.%Y').date(),
+        end_date=datetime.strptime(data.get('end_date'), '%d.%m.%Y').date(),
+        is_private=bool(data.get('is_private')),
+        host_id=current_user
+    )
+    db_sess.add(calendar)
+    db_sess.commit()
+    return jsonify({'success': True})
 
 
 # регистрация аккаунта
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
-    form = RegisterForm()
-    if request.method == "POST" and not form.validate_on_submit():
+    if request.method == "POST":
         db_sess = db_session.create_session()
         data = request.json.get('data')
         username = data.get('username')
         surname = data.get('surname')
-        name = data.form.get('name')
+        name = data.get('name')
         patronymic = data.get('patronymic')
         about = data.get('about')
         email = data.get('email')
         password = data.get('password')
         if db_sess.query(User).filter(User.username == username).first():
-            return {'success': 'user_exists'}
+            return {'success': False, 'Error': 'user_exists'}
         if db_sess.query(User).filter(User.email == email).first():
-            return {'success': 'email_exists'}
+            return {'success': False, 'Error': 'email_exists'}
         user = User(
             username=username,
             surname=surname,
@@ -76,66 +179,28 @@ def reqister():
         user.set_password(password)
         db_sess.add(user)
         db_sess.commit()
-        return jsonify({'success': "OK"})
-    db_sess = db_session.create_session()
-    if form.validate_on_submit():
-        form.username.data = str(form.username.data).lower().strip()
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Пароли не совпадают")
-
-        if db_sess.query(User).filter(User.username == form.username.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Такое имя пользователя уже есть")
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Такой адрес электронной почты уже есть")
-        if " " in form.username.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form, message="Нельзя использовать пробел в имени")
-        user = User(
-            username=form.username.data,
-            surname=form.surname.data,
-            name=form.name.data,
-            patronymic=form.patronymic.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-
-    return render_template('register.html', title='Регистрация', form=form)
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'success': True, 'access_token': access_token})
 
 
 # вход в аккаунт
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     data = request.json.get('data')
-    auth = data.get('auth')
+    email = data.get('email')
     password = data.get('password')
-    remember = data.get('remember')
     db_sess = db_session.create_session()
-    if "@" in str(auth):
-        user = db_sess.query(User).filter(User.email == auth).first()
-    else:
-        user = db_sess.query(User).filter(User.username == auth).first()
+    user = db_sess.query(User).filter(User.email == email).first()
     if user and user.check_password(password):
-        if bool(remember):
-            login_user(user, remember=True)
-        else:
-            login_user(user, remember=False)
-        return jsonify({'success': "OK"})
-    return jsonify({'success': "WrongAuth"})
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"access_token": access_token, "login": user.username}), 200
+    return jsonify({'success': False, "Error": "WrongAuth"})
 
 
-# выход из аккаунта
-@app.route('/logout')
-@login_required
+@app.route("/logout", methods=["POST"])
 def logout():
-    logout_user()
-    return redirect("/")
+    unset_jwt_cookies(jsonify({"Success": True}))
+    return jsonify({"Success": True})
 
 
 # переход в профиль
@@ -154,7 +219,7 @@ def profile(username):
 
 # изменение профиля
 @app.route("/profile/<string:username>/edit", methods=['GET', 'POST'])
-@login_required
+@jwt_required()
 def profile_edit(username):
     if request.method == "GET":
         db_sess = db_session.create_session()
@@ -178,9 +243,9 @@ def profile_edit(username):
             user.about = data.get('about')
             user.email = data.get('email')
             password = data.get('password')
-            if db_sess.query(User).filter(User.username == user.username).first() and not current_user:
+            if db_sess.query(User).filter(User.username == user.username).first():
                 return {'success': 'user_exists'}
-            if db_sess.query(User).filter(User.email == user.email).first() and not current_user:
+            if db_sess.query(User).filter(User.email == user.email).first():
                 return {'success': 'email_exists'}
             user.set_password(password)
 
@@ -193,49 +258,16 @@ def profile_edit(username):
 
 # удаление профиля
 @app.route("/profile/<string:username>/delete", methods=['GET', 'POST'])
-@login_required
-def news_delete(username):
+@jwt_required()
+def profile_delete(username):
     db_sess = db_session.create_session()
     user = db_sess.query(User).filter_by(username=username).first()
     if user:
         db_sess.delete(user)
         db_sess.commit()
     else:
-        return jsonify({'data': 'User not found'})
-    return jsonify({'success': "OK"})
-
-
-@app.route('/task/add', methods=['GET', 'POST'])
-def add_task():
-    db_sess = db_session.create_session()
-    data = request.json.get('data')
-    task_name = data.get('task_name')
-    host_id = current_user
-    begin_date = data.get('begin_date')
-    end_date = data.get('end_date')
-    is_private = data.get('is_private')
-    priority = data.get('priority')
-    description = data.get('description')
-    created_date = data.get('created_date')
-    task = Task(
-        task_name=task_name,
-        host_id=host_id,
-        begin_date=begin_date,
-        end_date=end_date,
-        is_private=is_private,
-        priority=priority,
-        description=description,
-        created_date=created_date
-    )
-    db_sess.add(task)
-    db_sess.commit()
-    return jsonify({'success': "OK"})
-
-
-@app.route('/test/Ilya', methods=['GET'])
-def test():
-    response = jsonify({'success': 'OK'})
-    return response
+        return jsonify({'success': False, 'Error': 'User not found'})
+    return jsonify({'success': True})
 
 
 if __name__ == '__main__':
