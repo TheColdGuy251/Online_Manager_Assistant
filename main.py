de13897cf49303df5838847572810fc908f82312
TheColdGuy251 <smirnovilya7031@gmail.com>
2024-04-30 19:53:17 +0500

    started working on optimisation

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sqlalchemy import text, or_, and_
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contacts import contact_blueprint
+from tasks import tasks_blueprint
 from flask_cors import CORS, cross_origin
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, jwt_manager, get_jwt, set_access_cookies, unset_jwt_cookies
 
@@ -23,6 +24,7 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=14)
 app.config['SQLALCHEMY_POOL_SIZE'] = 20
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
 app.register_blueprint(contact_blueprint)
+app.register_blueprint(tasks_blueprint)
 
 
 def main():
@@ -59,89 +61,6 @@ def refresh_expiring_jwts(response):
         return response
 
 
-@app.route("/tasks", methods=['GET'])
-@jwt_required()
-def load_task():
-    if request.method == "GET":
-        db_sess = db_session.create_session()
-        current_user_id = get_jwt_identity()
-        user = db_sess.query(User).filter(User.id == current_user_id).first()
-        if not user:
-            return jsonify({'success': False, 'error': "User not found"}), 404
-
-        # Retrieve tasks where the current user is either the host or a participant
-        tasks = db_sess.query(Task).filter((Task.host_id == current_user_id) | (Task.participants.any(user_id=current_user_id)) | (user.position == 0)).all()
-
-        data = []
-
-        for task in tasks:
-            formatted_begin_date, formatted_end_date, formatted_date_remind = task.formatted_dates()
-            task_particip = []
-            for particip in task.participants:
-                fio = db_sess.query(User).filter(User.id == particip.user_id).first()
-                task_particip.append({
-                    "id": particip.user_id,
-                    "label": fio.surname + " " + fio.name + " " + fio.patronymic
-                })
-
-            is_participant = current_user_id in task_particip
-            data.append({
-                "id": task.id,
-                "task_name": task.task_name,
-                "begin_date": formatted_begin_date,
-                "end_date": formatted_end_date,
-                "condition": task.condition,
-                "complete_perc": task.complete_perc,
-                "remind": task.remind,
-                "date_remind": formatted_date_remind,
-                "is_private": task.is_private,
-                "priority": task.priority,
-                "host_id": task.host_id,
-                "description": task.description,
-                "created_date": task.created_date,
-                "is_participant": is_participant,
-                "task_particip": task_particip
-            })
-
-        return jsonify({'success': True, 'data': data})
-
-
-@app.route("/tasks/add", methods=['POST'])
-@jwt_required()
-def add_task():
-    db_sess = db_session.create_session()
-    current_user = get_jwt_identity()
-    user = db_sess.query(User).filter(User.id == current_user).first()
-    if not user:
-        return jsonify({'success': False, 'error': "User not found"}), 404
-    data = request.json.get('data')
-    task = Task(
-        task_name=data.get('task_name'),
-        begin_date=datetime.strptime(data.get('begin_date'), '%Y-%m-%d').date(),
-        end_date=datetime.strptime(data.get('end_date'), '%Y-%m-%d').date(),
-        condition=data.get('condition'),
-        complete_perc=data.get('complete_perc'),
-        remind=bool(data.get('remind')),
-        date_remind=datetime.strptime(data.get('date_remind'), '%Y-%m-%d').date(),
-        is_private=bool(data.get('is_private')),
-        priority=data.get('priority'),
-        description=data.get('description'),
-        host_id=current_user
-    )
-    db_sess.add(task)
-    db_sess.commit()
-    task_participants = data.get('task_particip')
-    for participant in task_participants:
-        participant = participant.get("value")
-        task_partic = TaskParticip(
-            task_id=task.id,
-            user_id=participant
-        )
-        db_sess.add(task_partic)
-        db_sess.commit()
-    return jsonify({'success': True})
-
-
 def get_user_by_id(db_sess, user_id):
     """Helper function to get a user by ID."""
     return db_sess.get(User, user_id)
@@ -167,69 +86,6 @@ def get_contact_details(db_sess, contact, current_user_id):
             "about": other_user.about,
         }
     return None
-
-
-@app.route("/tasks/update", methods=['POST'])
-@jwt_required()
-def edit_task():
-    db_sess = db_session.create_session()
-    current_user = get_jwt_identity()
-    user = db_sess.query(User).filter(User.id == current_user).first()
-    if not user:
-        return jsonify({'success': False, 'error': "User not found"}), 404
-    data = request.json.get('data')
-    task_id = data.get('id')
-    host_id = data.get("host_id")
-    host_user = db_sess.query(User).filter(User.id == host_id).first()
-    task = db_sess.query(Task).filter(Task.id == task_id, or_(Task.host_id == current_user,
-                                                              host_user.position < user.position)).first()
-    if task:
-        task.task_name = data.get('task_name')
-        task.begin_date = datetime.strptime(data.get('begin_date'), '%Y-%m-%d').date()
-        task.end_date = datetime.strptime(data.get('end_date'), '%Y-%m-%d').date()
-        task.condition = data.get('condition')
-        task.complete_perc = data.get('complete_perc')
-        task.remind = bool(data.get('remind'))
-        task.date_remind = datetime.strptime(data.get('date_remind'), '%Y-%m-%d').date()
-        task.is_private = bool(data.get('is_private'))
-        task.priority = data.get('priority')
-        task.description = data.get('description')
-        db_sess.query(TaskParticip).filter(TaskParticip.task_id == task.id).delete()
-        task_participants = data.get('task_particip')
-        for participant in task_participants:
-            participant = participant.get("value")
-            task_partic = TaskParticip(
-                task_id=task.id,
-                user_id=participant
-            )
-            db_sess.add(task_partic)
-            db_sess.commit()
-
-        db_sess.commit()
-    return jsonify({'success': True})
-
-
-@app.route("/tasks/delete", methods=['POST'])
-@jwt_required()
-def delete_task():
-    db_sess = db_session.create_session()
-    current_user = get_jwt_identity()
-    user = db_sess.query(User).filter(User.id == current_user).first()
-    if not user:
-        return jsonify({'error': "User not found"}), 404
-    data = request.json.get('data')
-    task_id = data.get('id')
-    host_id = data.get("host_id")
-    host_user = db_sess.query(User).filter(User.id == host_id).first()
-    task = db_sess.query(Task).filter(Task.id == task_id, or_(Task.host_id == current_user,
-                                                              host_user.position < user.position)).first()
-    if task:
-        db_sess.query(TaskParticip).filter(TaskParticip.task_id == task.id).delete()
-        db_sess.delete(task)
-        db_sess.commit()
-    else:
-        abort(404)
-    return jsonify({'success': True})
 
 
 @app.route("/calendar", methods=['GET'])
@@ -299,8 +155,6 @@ def add_calendar():
     db_sess.add(calendar)
     db_sess.commit()
     return jsonify({'success': True})
-
-
 
 
 # регистрация аккаунта
